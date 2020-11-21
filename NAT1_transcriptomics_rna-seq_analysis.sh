a7aa6e8541c73c4b80d4bff996fade9ff3f2dc31
Samantha <samcarli@nmsu.edu>
2020-11-21 12:39:34 -0700

    Updating conda env and memory

--- NAT1_transcriptomics_rna-seq_analysis.sh
+++ NAT1_transcriptomics_rna-seq_analysis.sh
@@ -3,7 +3,7 @@
 #SBATCH --output rna-seq_snakemake.o%j ##filename of the output; the "%j" will append the jobID to the end of the name making the output files unique despite the sane job name; default is slurm-[jobID].out
 #SBATCH --partition normal ##the partition to run in [options: normal, gpu, debug]; default = normal
 #SBATCH	--nodes 1 ##number of nodes to use; default = 1
-#SBATCH --ntasks 3 ##number of tasks (analyses) to run; default = 1
+#SBATCH --ntasks 2 ##number of tasks (analyses) to run; default = 1
 #SBATCH --cpus-per-task 16 ##the number of threads the code will use; default = 1
 #SBATCH	--time 2-00:00:00 ##time for analysis (day-hour:min:sec) -- Max walltime will vary by partition; time formats are: "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds"
 #SBATCH --mail-user samcarli@nmsu.edu   ##your email address
@@ -27,4 +27,4 @@ set -o errexit
 set -x
 
 
-snakemake --cluster "sbatch -t {cluster.time} -p {cluster.partition} --mem {cluster.mem-per-cpu-mb}" --cluster-config cluster.slurm.cheaha.json --latency-wait=15 --use-conda --jobs 100
+snakemake --cluster "sbatch -t {cluster.time} -p {cluster.partition} -c {cluster.threads} --mem {cluster.mem-per-cpu-mb}" --cluster-config cluster.slurm.cheaha.json --latency-wait=15 --use-conda --jobs 100
